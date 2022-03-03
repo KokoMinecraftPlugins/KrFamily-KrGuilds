347686a33e622bc08eb239eb0299e3332f981e22
Kokored7214 <67258143+Kokored7214@users.noreply.github.com>
2022-03-03 20:28:58 +0800

    init plugin

---
+++ src/main/java/krmcplugins/kokored/website/advancedguild/AdvancedGuild.java
@@ -0,0 +1,17 @@
+package krmcplugins.kokored.website.advancedguild;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AdvancedGuild extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
