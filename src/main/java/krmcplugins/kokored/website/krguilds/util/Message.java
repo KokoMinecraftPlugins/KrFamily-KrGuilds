af3e2f4ad6e4acfe620add9296c493eef645640c
Kokored7214 <67258143+Kokored7214@users.noreply.github.com>
2022-03-06 16:03:42 +0800

    Update Message.java

--- src/main/java/krmcplugins/kokored/website/krguilds/util/Message.java
+++ src/main/java/krmcplugins/kokored/website/krguilds/util/Message.java
@@ -1,16 +1,26 @@
 package krmcplugins.kokored.website.krguilds.util;
 
+import krmcplugins.kokored.website.krguilds.KrGuilds;
+
 public class Message {
 
-    public final String PREFIX = "&f[&bKr&2Guilds&f] ";
+    KrGuilds krGuilds;
 
-    public final String COMMAND_NOT_FOUND = "COMMAND_NOT_FOUND";
-    public final String NO_PERMISSION = "NO_PERMISSION";
-    public final String REQUIRE_ARGS = "REQUIRE_ARGS";
+    public Message(KrGuilds krGuilds) {
+        this.krGuilds = krGuilds;
+    }
+
+    public final String PREFIX = Config.get().getString("PREFIX");
+
+    public final String COMMAND_NOT_FOUND = Config.get().getString("COMMAND_NOT_FOUND");
+    public final String NO_PERMISSION = Config.get().getString("NO_PERMISSION");
+    public final String REQUIRE_ARGS = Config.get().getString("REQUIRE_ARGS");
 
     // === Command ===
-    public final String G_ERROR = "G_ERROR";
-    public final String G_INFO_USAGE = "G_INFO_USAGE";
-    public final String G_CREATE_USAGE = "G_CREATE_USAGE";
+    public final String G_ERROR = Config.get().getString("G_ERROR");
+    public final String G_INFO_USAGE = Config.get().getString("G_INFO_DESC");
+    public final String G_CREATE_USAGE = Config.get().getString("G_CREATE_DESC");
+    public final String G_CREATE_NAME_EMPTY = Config.get().getString("G_CREATE_NAME_EMPTY");
+    public final String G_CREATE_DESC_EMPTY = Config.get().getString("G_CREATE_DESC_EMPTY");
     
 }
